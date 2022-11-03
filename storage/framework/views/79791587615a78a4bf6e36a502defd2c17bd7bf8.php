37e79413a63afeb58eacdef68322849f3ef525bc
Cas74l4n <63763425+Cas74l4n@users.noreply.github.com>
2022-11-02 22:48:00 -0600

    Add files via upload

---
+++ storage/framework/views/79791587615a78a4bf6e36a502defd2c17bd7bf8.php
@@ -0,0 +1,14 @@
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Tutorial de Bootstrap</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>El mejor tutorial de Bootstrap!</h1>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html><?php /**PATH C:\Users\caste\Escritorio\Respado Tareas 7to\Laravel\example-app\resources\views/welcome.blade.php ENDPATH**/ ?>
